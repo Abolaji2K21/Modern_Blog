d61f2773704e10142cc173d39490e15bbe5816b3
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-26 06:15:16 +0100

    Comment done

--- src/main/java/africa/semicolon/data/model/Comment.java
+++ src/main/java/africa/semicolon/data/model/Comment.java
@@ -1,4 +1,16 @@
 package africa.semicolon.data.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("Comments")
 public class Comment {
+    @Id
+    private String id;
+    @DBRef
+    private User commenter;
+    private String comment;
 }
