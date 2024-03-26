858648329b9906573fb1952286852a56509ce704
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-26 06:18:07 +0100

    View done

--- src/main/java/africa/semicolon/data/model/View.java
+++ src/main/java/africa/semicolon/data/model/View.java
@@ -1,4 +1,18 @@
 package africa.semicolon.data.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Document("Views")
 public class View {
+    @Id
+    private String id;
+    @DBRef
+    private User viewer;
+    private LocalDateTime timeOfView;
 }
