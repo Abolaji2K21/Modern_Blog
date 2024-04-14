4c66f5ff416e20a8005484233818f9ad3cdc2269
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-04-14 20:45:09 +0100

    comment controller

--- src/main/java/africa/semicolon/controllers/CommentController.java
+++ src/main/java/africa/semicolon/controllers/CommentController.java
@@ -2,18 +2,16 @@ package africa.semicolon.controllers;
 
 import africa.semicolon.BlogException.BigBlogException;
 import africa.semicolon.dtos.requests.CreateCommentRequest;
+import africa.semicolon.dtos.requests.DeleteCommentRequest;
+import africa.semicolon.dtos.requests.EditCommentRequest;
 import africa.semicolon.dtos.responds.ApiResponse;
 import africa.semicolon.services.CommentService;
 import africa.semicolon.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/comment")
@@ -22,12 +20,51 @@ public class CommentController {
     private CommentService commentService;
 
     @PatchMapping("/comment")
-    public ResponseEntity<?> addComment(@RequestBody CreateCommentRequest createCommentRequest) {
+    public ResponseEntity<?> CreateComment(@RequestBody CreateCommentRequest createCommentRequest) {
         try {
             var result = commentService.createCommentWith(createCommentRequest);
             return new ResponseEntity<>(new ApiResponse(true, result), CREATED);
         }
         catch (BigBlogException message) {
+            return new ResponseEntity<>(new ApiResponse(false, message.getMessage()), BAD_REQUEST);
+        }
+    }
+    @PatchMapping("/edit-comment")
+    public ResponseEntity<?> editComment(@RequestBody EditCommentRequest editCommentRequest) {
+        try {
+            var result = commentService.editCommentWith(editCommentRequest);
+            return new ResponseEntity<>(new ApiResponse(true, result), CREATED);
+        } catch (BigBlogException message) {
+            return new ResponseEntity<>(new ApiResponse(false, message.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/delete-comment")
+    public ResponseEntity<?> deleteComment(@RequestBody DeleteCommentRequest deleteCommentRequest) {
+        try {
+            var result = commentService.deleteCommentWith(deleteCommentRequest);
+            return new ResponseEntity<>(new ApiResponse(true, result), CREATED);
+        } catch (BigBlogException message) {
+            return new ResponseEntity<>(new ApiResponse(false, message.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/get-comments/{postId}")
+    public ResponseEntity<?> getCommentsByPost(@PathVariable(name = "postId") String postId) {
+        try {
+            var result = commentService.getCommentBy(postId);
+            return new ResponseEntity<>(new ApiResponse(true, result), OK);
+        } catch (BigBlogException message) {
+            return new ResponseEntity<>(new ApiResponse(false, message.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/get-comments/{postId}/{userId}")
+    public ResponseEntity<?> getCommentsByUser(@PathVariable(name = "postId") String postId, @PathVariable(name = "userId") String userId) {
+        try {
+            var result = commentService.getCommentsByUser(postId, userId);
+            return new ResponseEntity<>(new ApiResponse(true, result), OK);
+        } catch (BigBlogException message) {
             return new ResponseEntity<>(new ApiResponse(false, message.getMessage()), BAD_REQUEST);
         }
     }
